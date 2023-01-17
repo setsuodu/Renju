8e6e09034938ec9e005f22e40b93e8e076d1decd
Xue <setsuodu@users.noreply.github.com>
2023-01-17 11:08:39 +0800

    调试Hotfix

--- xLua/Assets/PureXLua/Scripts/Test/TestLuaCallCS.cs
+++ xLua/Assets/PureXLua/Scripts/Test/TestLuaCallCS.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
-//[LuaCallCSharp] //不加也可以调用
+// 注意，这里添加的LuaCallCSharp特性只是为了使xLua为其生成代码，不添加并不影响功能
+[XLua.LuaCallCSharp] //不加也可以调用？
+[XLua.Hotfix]
 public class TestLuaCallCS : MonoBehaviour
 {
     public int index;
